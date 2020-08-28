0268e091656d9345d440ab16b7ab9e2a4553affd
alpha <123@qq.com>
2020-08-28 16:53:08 +0800

    music

--- qqmusic/src/App.vue
+++ qqmusic/src/App.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-        <h1>11111</h1>
+        <m-header></m-header>
     </div>
 </template>
+
+<script>
+import MHeader from './components/m-header/m-header'
+export default {
+    components:{
+        MHeader
+    }
+}
+</script>
